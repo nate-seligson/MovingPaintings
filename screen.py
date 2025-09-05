a8314b33c573eeffa17ff8ec3a6065c4daeb8dac
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2025-09-05 12:29:02 -0500

    fix

--- screen.py
+++ screen.py
@@ -10,23 +10,20 @@ class VideoWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # --- EDIT THIS to set the video file path ---
         self.video_file = "/home/pil/Downloads/MovingPaintings/65562-515098354_medium.mp4"
-        # ------------------------------------------------------------------
 
-        # --- EDIT THESE to set transform in code ---
+        # Transform params
         self.video_x = 0
         self.video_y = 0
         self.video_rotation = 0
         self.scale_x = 1.0
         self.scale_y = 1.0
-        # ------------------------------------------------------------------
 
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('')
-        self.showFullScreen()  # fullscreen window
+        self.showFullScreen()
         self.setStyleSheet("background-color: black;")
 
         # Graphics view + scene
@@ -39,6 +36,10 @@ class VideoWindow(QMainWindow):
         self.graphics_scene.setBackgroundBrush(QColor("black"))
         self.graphics_view.setScene(self.graphics_scene)
 
+        # Video item
+        self.video_item = QGraphicsVideoItem()
+        self.graphics_scene.addItem(self.video_item)
+
         # Media player + playlist
         self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.playlist = QMediaPlaylist()
@@ -47,22 +48,18 @@ class VideoWindow(QMainWindow):
         self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(self.video_file)))
         self.playlist.setCurrentIndex(0)
 
-        # Video item
-        self.video_item = QGraphicsVideoItem()
-        self.graphics_scene.addItem(self.video_item)
         self.media_player.setVideoOutput(self.video_item)
 
-        # Wait for media to load to get pixel size
-        self.media_player.mediaStatusChanged.connect(self.on_media_status_changed)
+        # Wait for metadata to be available
+        self.media_player.metaDataChanged.connect(self.on_metadata_changed)
         self.media_player.play()
 
-    def on_media_status_changed(self, status):
-        if status == QMediaPlayer.LoadedMedia:
-            # Set the video to its native pixel size
-            video_size = self.media_player.metaData("Resolution")
-            if video_size is not None:
-                self.video_item.setSize(QSizeF(video_size.width(), video_size.height()))
-                self.apply_transformations()
+    def on_metadata_changed(self):
+        # Only proceed if resolution is available
+        video_size = self.media_player.metaData("Resolution")
+        if video_size is not None:
+            self.video_item.setSize(QSizeF(video_size.width(), video_size.height()))
+            self.apply_transformations()
 
     def apply_transformations(self):
         transform = QTransform()
@@ -71,10 +68,9 @@ class VideoWindow(QMainWindow):
         transform.scale(self.scale_x, self.scale_y)
         self.video_item.setTransform(transform)
 
-    # Override resizeEvent but do nothing to prevent scaling with window
+    # Keep video pixel size constant on resize
     def resizeEvent(self, event):
         super().resizeEvent(event)
-        # Keep the video pixel size constant
         self.apply_transformations()
 
 
